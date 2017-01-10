56a103767632e7da88131f6300d36a7a157a7cde
Imtiaz Buckas <imtiaz.pocketpc@gmail.com>
2017-01-10 22:31:46 +0200

    Android Splash Screen functional

---
+++ MediaFarmer.MobileDevice/MediaFarmer.MobileDevice.Droid/SplashActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Threading.Tasks;
+using Android.Content;
+
+namespace MediaFarmer.MobileDevice.Droid
+{
+    [Activity(Label = "MediaFarmer.MobileDevice", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            System.Threading.Thread.Sleep(3000); //Let's wait awhile...
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
+
---
+++ MediaFarmer.MobileDevice/MediaFarmer.MobileDevice.Droid/SplashScreenReadMe.txt
@@ -0,0 +1,2 @@
+﻿https://developer.xamarin.com/Guides/Android/User_Interface/Creating_a_Splash_Screen/
+http://codeworks.it/blog/?p=294
